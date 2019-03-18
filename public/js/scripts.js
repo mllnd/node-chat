34a4dd89164b03f8aba8aa8a4b27cc1d06036c28
Markkus Millend <markusmillend@me.com>
2019-03-19 00:05:56 +0200

    Add username input form

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -7,9 +7,33 @@ $('#chat-message-form').submit(function(event) {
         $('input[name="message"]').val('');
     }
 });
+$('#nickname-form').submit(function(event) {
+    var socket = io();
+    var nickname = $('input[name="nickname"]').val();
+    event.preventDefault();
+    if (nickname.length > 0) {
+        $('input[name="nickname"]').attr('disabled', 'disabled');
+        animate('.nickname-input-wrapper', 'fadeOutUp', function() {
+            $('.nickname-input').hide();
+            $('.chat-cover').fadeIn();
+        })
+        // $('input[name="nickname"]').val('');
+    }
+});
 socket.on('chat-message', function(message) {
     $wrapper = $('#chat-wrapper');
     var div = $('<div>', {class: 'chat-entry'}).append($('<span>', {class: 'chat-entry-text'}).text(message));
     $('#chat').append(div);
     $wrapper.animate({scrollTop: $wrapper[0].scrollHeight}, 'slow');
 });
+
+function animate(element, animationName, callback) {
+    const node = document.querySelector(element)
+    node.classList.add('animated', animationName)
+    function handleAnimationEnd() {
+        node.classList.remove('animated', animationName)
+        node.removeEventListener('animationend', handleAnimationEnd)
+        if (typeof callback === 'function') callback()
+    }
+    node.addEventListener('animationend', handleAnimationEnd)
+}
