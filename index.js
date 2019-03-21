c3dfd03320e14b1a03d897580aca1acddc0d17df
Markkus Millend <markusmillend@me.com>
2019-03-21 02:37:25 +0200

    Display active users on the left sidebar

--- index.js
+++ index.js
@@ -5,13 +5,43 @@ var io = require('socket.io').listen(server);
 var port = 3000;
 
 // Serve files from public folder
-app.use(express.static('public'))
+app.use(express.static('public'));
+
+var user_count = 0;
+var user_list = [];
+
+app.get('/users', function(request, response) {
+    response.send(user_list);
+});
 
 io.on('connection', function(socket) {
+    var connected_user = false;
     socket.on('chat-message', function(message) {
         io.emit('chat-message', message);
         // console.log('Message: '+message);
     });
+    socket.on('user-join', function(nickname) {
+        if (connected_user) return;
+        socket.nickname = nickname;
+        ++user_count;
+        user_list.push(nickname);
+        connected_user = true;
+        io.emit('user-login', {
+            'nickname': nickname,
+            'user_count': user_list.length
+        });
+    });
+    socket.on('disconnect', function() {
+        if (connected_user) {
+            --user_count;
+            connected_user = false;
+            user_list.splice(user_list.indexOf(socket.nickname), 1);
+            io.emit('user-logout', {
+                'nickname': socket.nickname,
+                'user_count': user_list.length
+            });
+        }
+    })
 });
 
 server.listen(port, () => console.log('Starting chat app on port '+port+'!'));
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,39 +1,80 @@
-$('#chat-message-form').submit(function(event) {
+$(function() {
+    // Initialize variables
+    var user_count = 0;
+    var user_list = [];
     var socket = io();
-    var message = $('input[name="message"]').val();
-    event.preventDefault();
-    if (message.length > 0) {
-        socket.emit('chat-message', $('input[name="message"]').val());
-        $('input[name="message"]').val('');
-    }
-});
-$('#nickname-form').submit(function(event) {
-    var socket = io();
-    var nickname = $('input[name="nickname"]').val();
-    event.preventDefault();
-    if (nickname.length > 0) {
-        $('input[name="nickname"]').attr('disabled', 'disabled');
-        animate('.nickname-input-wrapper', 'fadeOutUp', function() {
-            $('.nickname-input').hide();
-            $('.chat-cover').fadeIn();
-        })
-        // $('input[name="nickname"]').val('');
-    }
-});
-socket.on('chat-message', function(message) {
-    $wrapper = $('#chat-wrapper');
-    var div = $('<div>', {class: 'chat-entry'}).append($('<span>', {class: 'chat-entry-text'}).text(message));
-    $('#chat').append(div);
-    $wrapper.animate({scrollTop: $wrapper[0].scrollHeight}, 'slow');
-});
+    // Set the user count
+    $('#user-count').text(user_count);
 
-function animate(element, animationName, callback) {
-    const node = document.querySelector(element)
-    node.classList.add('animated', animationName)
-    function handleAnimationEnd() {
-        node.classList.remove('animated', animationName)
-        node.removeEventListener('animationend', handleAnimationEnd)
-        if (typeof callback === 'function') callback()
+    $('#chat-message-form').submit(function(event) {
+        var message = $('input[name="message"]').val();
+        event.preventDefault();
+        if (message.length > 0) {
+            socket.emit('chat-message', message);
+            $('input[name="message"]').val('');
+        }
+    });
+
+    $('#nickname-form').submit(function(event) {
+        var nickname = $('input[name="nickname"]').val();
+        event.preventDefault();
+        if (nickname.length > 0) {
+            $('input[name="nickname"]').attr('disabled', 'disabled');
+            socket.emit('user-join', nickname);
+            getUsers();
+            animate('.nickname-input-wrapper', 'fadeOutUp', function() {
+                $('.nickname-input').hide();
+                $('.chat-cover').fadeIn();
+            })
+            // $('input[name="nickname"]').val('');
+        }
+    });
+
+    socket.on('user-login', function(data) {
+        $('#user-count').text(data.user_count);
+        if ($.inArray(data.nickname, user_list) == -1) {
+            user_list.push(data.nickname);
+            var li = $('<li>', {nickname: data.nickname}).append($('<a>', {href: '#'}).text(data.nickname));
+            $('.nav.user-list').append(li)
+        }
+    });
+
+    socket.on('user-logout', function(data) {
+        $('#user-count').text(data.user_count);
+        user_list.splice(user_list.indexOf(data.nickname), 1);
+        $('.nav.user-list li[nickname="'+data.nickname+'"]').remove();
+    })
+
+    socket.on('chat-message', function(message) {
+        $wrapper = $('#chat-wrapper');
+        var div = $('<div>', {class: 'chat-entry'}).append($('<span>', {class: 'chat-entry-text'}).text(message));
+        $('#chat').append(div);
+        $wrapper.animate({scrollTop: $wrapper[0].scrollHeight}, 'slow');
+    });
+
+    function getUsers() {
+        $.get('/users', function(data) {
+            if (data.length > 0) {
+                for (var i = 0; i < data.length; i++) {
+                    // Check if user already exists in local user list
+                    if ($.inArray(data[i], user_list) == -1) {
+                        user_list.push(data[i]);
+                        var li = $('<li>', {nickname: data[i]}).append($('<a>', {href: '#'}).text(data[i]));
+                        $('.nav.user-list').append(li)
+                    }
+                }
+            }
+        });
     }
-    node.addEventListener('animationend', handleAnimationEnd)
-}
+
+    function animate(element, animationName, callback) {
+        const node = document.querySelector(element)
+        node.classList.add('animated', animationName)
+        function handleAnimationEnd() {
+            node.classList.remove('animated', animationName)
+            node.removeEventListener('animationend', handleAnimationEnd)
+            if (typeof callback === 'function') callback()
+        }
+        node.addEventListener('animationend', handleAnimationEnd)
+    }
+});
